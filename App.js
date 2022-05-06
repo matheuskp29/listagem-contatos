06b369c07effd203e642e7e7665ef733082364fa
Matheus Kaczan <matheuskp29@gmail.com>
2022-05-05 22:26:01 -0300

    Funcoes funcionando

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
     const [nome, setNome] = useState('');
@@ -17,8 +17,8 @@ export default function App() {
 
     const adicionarContato = () => {
         setContatos(contatos => {
-            setContador(contador + 1);
-            let aux = [{ key: contador.toString(), values: nome, telefone }, ...contatos];
+            setContador(contador + 1)
+            let aux = [...contatos, { key: contador.toString(), value: {nome: nome, telefone: telefone} } ];
             setNome('');
             setTelefone('');
             console.log(aux);
@@ -35,12 +35,13 @@ export default function App() {
                     onChangeText = {capturarNome}
                     value={nome}
                 />
-                <TextInput 
+                <TextInput
+                    style={styles.inputText}
                     placeholder="Digite o telefone"
                     onChangeText = {capturarTelefone}
                     value={telefone}
                 />
-                <Button 
+                <Button
                     disabled={nome.length === 0 || telefone.length === 0}
                     title="Adicionar Contato"
                     onPress={adicionarContato}
@@ -52,12 +53,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
     telaPrincipalView: {
-        padding: 40
+        padding: 40,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
     },
     inputText: {
         borderBottomColor: 'black',
         borderBottomWidth: 2,
         marginBottom: 4,
-        padding: 12
+        padding: 12,
     },
 });
